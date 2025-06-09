c1070be7c460f23720ec26e177dd64f840f2bdca
angelapaulam <angelapaulamarques@gmail.com>
2025-06-08 23:54:26 -0300

    Execucao Trabalho Final 17

--- testes/trabalho-final.js
+++ testes/trabalho-final.js
@@ -1,5 +1,3 @@
-import assert from "node:assert";
-
 import {
   geradorDeTagsDeIdentificacao,
   verificarSePodeSerAdotado,
@@ -9,27 +7,26 @@ import {
 } from "../trabalho.js";
 
 describe("Testes da disciplina - fundamentos JS", () => {
-  it("QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas", () => {
-    assert.strictEqual(geradorDeTagsDeIdentificacao("Pantera"), "PANTERA");
+  test("QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas", () => {
+    expect(geradorDeTagsDeIdentificacao("Pantera")).toBe("PANTERA");
   });
 
-  it("QUANDO a idade = 1 + porte M, DEVE ser permitida a adoção", () => {
-    assert.strictEqual(verificarSePodeSerAdotado(1, "M"), true);
+  test("QUANDO a idade = 1 + porte M, DEVE ser permitida a adoção", () => {
+    expect(verificarSePodeSerAdotado(1, "M")).toBe(true);
   });
 
-  it("QUANDO o peso = 14.5, DEVE ser retornado 4350 gramas para o consumo diário", () => {
-    assert.strictEqual(calcularConsumoDeRacao("Pitoco", 1, 14.5), 4350);
+  test("QUANDO o peso = 14.5, DEVE ser retornado 4350 gramas para o consumo diário", () => {
+    expect(calcularConsumoDeRacao("Pitoco", 1, 14.5)).toBe(4350);
   });
 
-  it("QUANDO o porte = pequeno, DEVE ser retornada a atividade adequada", () => {
-    assert.strictEqual(
-      decidirTipoDeAtividadePorPorte("pequeno"),
-      "brincar dentro de casa",
+  test("QUANDO o porte = pequeno, DEVE ser retornada a atividade adequada", () => {
+    expect(decidirTipoDeAtividadePorPorte("pequeno")).toBe(
+      "brincar dentro de casa"
     );
   });
 
-  it("QUANDO buscar dado de exemplo, DEVE retornar um valor de forma assíncrona", async () => {
+  test("QUANDO buscar dado de exemplo, DEVE retornar um valor de forma assíncrona", async () => {
     const resultado = await buscarDadoAsync();
-    assert.strictEqual(resultado, "Pipoca");
+    expect(resultado).toBe("Pipoca");
   });
 });
